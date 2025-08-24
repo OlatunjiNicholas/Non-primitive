761af8e1e46e4aca070d2116f747d47364555b31
Your name <OlatunjiNicholas.com>
2025-08-24 17:04:19 +0100

    third push

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const ball = 12
+
+console.log(ball % 2)
+if(ball === 12)(
+    console.log("number is even")
+)
+
+const cub = 21
+
+console.log(cub % 2)
+if(cub === 21)(
+    console.log("number is odd")
+)
+
+
+
+
+
+
