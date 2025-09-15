f0f22a7b6630aa798aacec16c3ad290b0fb3f81d
Your name <OlatunjiNicholas.com>
2025-09-15 21:04:29 +0100

    fifth commit

---
+++ Loop.js
@@ -0,0 +1,9 @@
+let house = 1  //starting point
+while(house <=20){  //condition to stop
+    if (house === 1){
+    console.log(house + " house")
+}else
+    console.log(house + " houses")    
+    house = house + 2 //how to iterate
+}
+ 
