f8354ebd66c94a1b32b899121b6a97e392151917
Your name <OlatunjiNicholas.com>
2025-09-16 12:17:17 +0100

    second array commit

---
+++ ArrayB.js
@@ -0,0 +1,10 @@
+// Accessing Element in Array
+const friends = ["Kola", "Tayo", "David", "Tunji", "Marvelous"]
+console.log(friends)
+console.log(friends[0])
+console.log(friends[1])
+console.log(friends[2])
+console.log(friends[3])
+console.log(friends[4])
+const bestFriend = friends[friends.length -5]
+console.log(bestFriend)
