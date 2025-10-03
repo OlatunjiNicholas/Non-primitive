fad5eb1b5f00f925da77e9a46a43e8404b533537
Your name <OlatunjiNicholas.com>
2025-10-03 16:22:03 +0100

    Object.js commit

---
+++ Object.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "Be Humble",
+    Description: "Humanity life style and the act of been response",
+    page: 100,
+    author: "Dada",
+    reading: false
+}
+console.log(book)
+    title: "Be Humble"
+    description: ""
