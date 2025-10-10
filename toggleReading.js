b5298802e8cd7d614aeb7398ffc29318e556ea17
Your name <OlatunjiNicholas.com>
2025-10-10 12:40:23 +0100

    toggle commit

---
+++ toggleReading.js
@@ -0,0 +1,17 @@
+function toggleReadingStatus() {
+  console.log(toggleReadingStatus)
+}
+  let book1 = {
+  title: "mathematic",
+  author: "fraction",
+  reading: false,
+  toggleReadingStatus
+};
+  let book2 = {
+  title: "1984",
+  author: "history",
+  reading: true,
+  toggleReadingStatus
+  }
+book1.toggleReadingStatus();
+book2.toggleReadingStatus();
