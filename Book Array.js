e7d30ee156f742a2f06c3c6c8c36f3f2d23ddd18
Your name <OlatunjiNicholas.com>
2025-10-22 16:43:22 +0100

    second commit

---
+++ Book Array.js
@@ -0,0 +1,33 @@
+// function myPersonalLibrary
+const book1 =[
+    title= "loyality",
+    description="king life sytle",
+    page= "100",
+    author= "olaiyifa",
+    reading= false,
+];
+
+const book2 = [
+    title= "rules",
+    description= "guide line to survive",
+    page= "70",
+    author= "olatunji",
+    reading = true, 
+];
+
+const book3 =[
+    title= "power",
+    description="rules of length",
+    page= "100",
+    author= "Adesoye",
+    reading= true,
+];
+const library=[...book1,...book2,...book3,"more",];
+console.log(library)
+    if(reading= true)
+        console.log(true)
+    else(
+        console.log(false)
+    )
+
+
