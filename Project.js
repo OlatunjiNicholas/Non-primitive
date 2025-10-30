353d39045a3c581d75c5a9610db3f069117b44df
Your name <OlatunjiNicholas.com>
2025-10-30 21:15:16 +0100

    correct project

--- Project.js
+++ Project.js
@@ -1,6 +1,6 @@
 // // 1.calculate the sum of number in array
-const sum = (a,b,c,d,e) => (a+b+c+d+e);
-console.log(sum(2,4,6,8,10))
+const total = (a,b,c,d,e) => (a+b+c+d+e);
+console.log(total(2,4,6,8,10))
 
 //             // or
 
@@ -9,39 +9,39 @@ function sum(a,b,c,d,e){
 }
 sum(2,3,4,5,6)
 
-// //2.create a length converter function
+// // //2.create a length converter function
 const meter3centimeter = (meters) => meters * 20
 console.log(meter3centimeter (10))
 
-// 3.print all even numbers from 0-100
+// // 3.print all even numbers from 0-100
 for (let h= 0; h<=100; h+=2)(
-    console.log(h +  'h')
+    console.log(h +' h')
 )
 
-// // // 4.print table containing multiple tables
-function multiple(a,b){
+// // // // 4.print table containing multiple tables
+function multiples(a,b){
     return a*b
 }
-let multiplication = multiple(10,3)
+let multiplication = multiples(10,3)
 console.log(multiplication)
 
-//             // or
+// //             // or
 
 let multiple = (d,e) => (d*e);
 console.log(multiple(6,9))
 
-// // 5. create a function that reverse array
+// // // 5. create a function that reverse array
 const array = ["bola", "tayo", "adeola", 100, 694];
 array.reverse();
 console.log(array)
 
-// // 6. sort an array of string in alphabetical order
+// // // 6. sort an array of string in alphabetical order
 const unsorted = ["H", "g", "C", "D", "E", "F","A","B"];
 console.log(unsorted)
 unsorted.sort();
 console.log(unsorted)
 
-// //              or
+// // //              or
 const unsort = ["H", "g", "C", "D", "E", "F","A","B"];
 console.log(unsort)
 const sorted = unsort.sort(
@@ -49,13 +49,13 @@ const sorted = unsort.sort(
 );
 console.log({sorted})
 
-// 7. sort an array number in descending order
+// // 7. sort an array number in descending order
 const number = [1, 2,3, 4, 5, 6,];
 console.log(number)
 number.reverse();
 console.log(number)
 
-//8. Return a boolean if a number is divided by 10
+// //8. Return a boolean if a number is divided by 10
 let data = (g,h) => (g/h)
     console.log(data(200,10))
 if(20,10){
@@ -65,12 +65,12 @@ else{
     console.log("false")
 }
 
-// // 9. Return the number of vowels in string 
+// // // 9. Return the number of vowels in string 
 const vowels = ['a','e','i','o','u'];
 console.log(vowels)
 console.log(vowels.length)
 
-// 10. create a function that filter out negative numbers
+// // 10. create a function that filter out negative numbers
 let numbers = [12, -43, -10,-21,45,44,55];
 numbers = numbers.filter(function(numbers){
     return numbers >-1
