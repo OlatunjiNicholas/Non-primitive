48154ded70c49a3aa77106c74125b5f5d6c49084
Your name <OlatunjiNicholas.com>
2025-11-01 04:54:18 +0100

    for loop

--- For Loop.js
+++ For Loop.js
@@ -1,11 +1,19 @@
 // FOR LOOP
-for(let house = 1; house <=20; house = house+=2)(
+for(let house = 1; house <=20; house = house+2){
+    if(house<=1)
     console.log(house + " house")
-)
+else{
+    console.log(house +" houses")
+}
+}
 // or
 for(let house = 1; 
     house <=20; 
-    house = house+=2)(
+    house = house+=2){
+        if(house<=1)
     console.log(house + " house")
-    )
+else{
+    console.log(house +" houses")
+}
+    }
 
